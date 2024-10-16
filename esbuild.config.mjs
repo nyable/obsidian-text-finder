273f92fdd9c96a70f8f62bd28d844a881713be53
nyable <loser@88.com>
2024-10-16 09:13:48 +0800

    恢复默认路径

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -50,7 +50,7 @@ const context = await esbuild.context({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outdir: "C:/Users/refan/Documents/Obsidian Vault/.obsidian/plugins/obsidian-text-finder",
+	outdir: "dist",
 	minify: prod,
 });
 
